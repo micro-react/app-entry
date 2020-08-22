da6bb3d5a38d8b35f465304bb6bac77be4c364a5
iWuzhi <2461358597@qq.com>
2020-08-22 20:50:43 +0800

    init: Add async Component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,17 @@
  * author iWuzhi
  * date 2020-08-22 10:58:08
  */
+// @ts-nocheck
+import React, { useState, useEffect } from 'react';
+import AsyncLoader from '../components/AsyncLoader/index';
 
-import HelloWord from '../components/HelloWorld/index';
-
-export default HelloWord;
+export default () => {
+  const [YourApp, setYourApp] = useState(null);
+  useEffect(() => {
+    AsyncLoader({ appName: 'your-app-name'}).then(Com => {
+      setYourApp(<Com />);
+    });
+  }, []);
+  if (YourApp === null) return <h1>loading...</h1>;
+  return YourApp;
+};
